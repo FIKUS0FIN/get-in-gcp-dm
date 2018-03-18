94875c38e125c157cf7e6ffc49bae5d027081d35
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2018-03-18 19:51:39 +0200

    add test build with gcp group

---
+++ splunk-cluster-group/start-up-scripts/splunk-node.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt update && apt upgrade -y && apt install -y htop
+wget -O splunk-7.0.2-03bbabbd5c0f-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.0.2&product=splunk&filename=splunk-7.0.2-03bbabbd5c0f-linux-2.6-amd64.deb&wget=true'
+sudo dpkg -i splunk*.deb
+cd /opt/splunk
+./bin/splunk start --accept-license
+./bin/splunk enable boot-start
+./bin/splunk edit cluster-config -mode master -replication_factor 4 -search_factor 3 -secret your_key -cluster_label cluster1  -auth admin:changeme
+./bin/splunk restart 
