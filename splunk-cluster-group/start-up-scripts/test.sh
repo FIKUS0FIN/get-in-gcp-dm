d8e26ff7641868a7a03de4c45559286b1ba4866c
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2018-03-26 13:05:36 +0300

    pilot ver 0.6_1

--- splunk-cluster-group/start-up-scripts/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-apt update && apt upgrade -y  && apt install nginx -y && apt install -y htop 
-wget "https://github.com/FIKUS0FIN/get-in-gcp-dm/blob/master/splunk-cluster-group/vm-configs/compute-engine-template.jinja"
